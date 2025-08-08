023c10e455b5aff2594c7cd389ca6252028c2fc1
baseballclub <abkimdoyo25@gmail.com>
2025-08-08 18:31:32 +0900

    :gear: Settings

---
+++ src/main/java/com/likelion/friendpass/FriendPassApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.friendpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendPassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendPassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/friendpass/FriendPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.friendpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendPassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
