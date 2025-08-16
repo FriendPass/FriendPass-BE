20a0042587761c477733fd964f4e1448d37a1a60
baseballclub <abkimdoyo25@gmail.com>
2025-08-16 16:30:12 +0900

    [Feat] Add certification db

--- src/main/java/com/likelion/friendpass/domain/certification/CertificationRepository.java
+++ src/main/java/com/likelion/friendpass/domain/certification/CertificationRepository.java
@@ -1,12 +1,12 @@
 package com.likelion.friendpass.domain.certification;
 
 import com.likelion.friendpass.domain.place.Place;
+import com.likelion.friendpass.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
 public interface CertificationRepository extends JpaRepository<Certification, Long> {
-    Optional<Certification> findByCertifiedDateAndUserAndPlace(LocalDate certificatedDate, User user, Place place);
+    Optional<Certification> findByCertifiedDateAndUserAndPlace(LocalDate certifiedDate, User user, Place place);
 }
