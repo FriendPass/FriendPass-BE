222e5a0faced326f71cbc68ce201f716d1eb99c7
baseballclub <abkimdoyo25@gmail.com>
2025-08-16 16:30:47 +0900

    [Refactor] Edit MatchingMemberRepository

--- src/main/java/com/likelion/friendpass/domain/matching/MatchingMemberRepository.java
+++ src/main/java/com/likelion/friendpass/domain/matching/MatchingMemberRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MatchingMemberRepository extends JpaRepository<MatchingMember, Long> {
-    List<MatchingMember> findByTeam_teamId(long teamId);
+    List<MatchingMember> findByTeam(MatchingTeam teamId);
 }
