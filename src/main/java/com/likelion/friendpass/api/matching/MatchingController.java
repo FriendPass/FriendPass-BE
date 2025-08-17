84082c7f381a31e80cbc43a16e70c7dace6a42d9
baseballclub <abkimdoyo25@gmail.com>
2025-08-17 17:45:57 +0900

    [Feat] Matching Service

--- src/main/java/com/likelion/friendpass/api/matching/MatchingController.java
+++ src/main/java/com/likelion/friendpass/api/matching/MatchingController.java
@@ -1,8 +1,8 @@
 package com.likelion.friendpass.api.matching;
 
-import com.likelion.friendpass.api.matching.dto.MatchingCompleteDto;
-import com.likelion.friendpass.api.matching.dto.MatchingRequestCreateDto;
-import com.likelion.friendpass.api.matching.dto.MatchingStatusDto;
+import com.likelion.friendpass.api.matching.dto.MatchingCompleteResponse;
+import com.likelion.friendpass.api.matching.dto.MatchingRequestCreate;
+import com.likelion.friendpass.api.matching.dto.MatchingStatusResponse;
 import com.likelion.friendpass.domain.matching.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -16,13 +16,13 @@ public class MatchingController {
 
     // 매칭 상태 확인 (신청 전 / 대기중)
     @GetMapping("/status")
-    public MatchingStatusDto getStatus(@RequestParam Long userId) {
+    public MatchingStatusResponse getStatus(@RequestParam Long userId) {
         return matchingService.getWaitingStatus(userId);
     }
 
     // 매칭 신청하기
     @PostMapping("/request")
-    public void createMatchingRequest(@RequestBody MatchingRequestCreateDto dto) {
+    public void createMatchingRequest(@RequestBody MatchingRequestCreate dto) {
         matchingService.createMatchingRequest(dto);
     }
 
@@ -34,7 +34,7 @@ public class MatchingController {
 
     // 매칭 완료 화면
     @GetMapping("/complete")
-    public MatchingCompleteDto getComplete(@RequestParam Long teamId) {
+    public MatchingCompleteResponse getComplete(@RequestParam Long teamId) {
         return matchingService.getMatchingComplete(teamId);
     }
 
