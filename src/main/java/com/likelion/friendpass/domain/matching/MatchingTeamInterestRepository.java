3350519c6b8f8f06f1b7011347cb9eae4e1b97d7
baseballclub <abkimdoyo25@gmail.com>
2025-08-17 17:49:12 +0900

    [Feat] Add findByTeam_TeamId

--- src/main/java/com/likelion/friendpass/domain/matching/MatchingTeamInterestRepository.java
+++ src/main/java/com/likelion/friendpass/domain/matching/MatchingTeamInterestRepository.java
@@ -2,5 +2,8 @@ package com.likelion.friendpass.domain.matching;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MatchingTeamInterestRepository extends JpaRepository<MatchingTeamInterest, Long> {
+    List<MatchingTeamInterest> findByTeam_TeamId(Long teamId);
 }
