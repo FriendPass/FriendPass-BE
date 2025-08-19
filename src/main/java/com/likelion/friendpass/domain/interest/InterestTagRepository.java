31fe1a01a3cc3ab20e451634e07c84c066a51bb9
baseballclub <abkimdoyo25@gmail.com>
2025-08-20 01:00:25 +0900

    [Feat] Add findBy

--- src/main/java/com/likelion/friendpass/domain/interest/InterestTagRepository.java
+++ src/main/java/com/likelion/friendpass/domain/interest/InterestTagRepository.java
@@ -2,8 +2,12 @@ package com.likelion.friendpass.domain.interest;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface InterestTagRepository extends JpaRepository<InterestTag, Long> {
     List<InterestTag> findAllByOrderByInterestIdAsc();
     long countByInterestIdIn(List<Long> ids);
+
+    Optional<InterestTag> findByName(String name);
+
 }
