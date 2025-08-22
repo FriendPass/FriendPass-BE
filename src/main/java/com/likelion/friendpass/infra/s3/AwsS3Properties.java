3889d51ce4708ed79dc25a03c0eb83168dfa9a14
baseballclub <abkimdoyo25@gmail.com>
2025-08-23 00:29:58 +0900

    [Fix] AwsS3Properties

--- src/main/java/com/likelion/friendpass/infra/s3/AwsS3Properties.java
+++ src/main/java/com/likelion/friendpass/infra/s3/AwsS3Properties.java
@@ -3,9 +3,11 @@ package com.likelion.friendpass.infra.s3;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
+@Component
 @ConfigurationProperties(prefix = "aws.s3")
 public class AwsS3Properties {
     private String bucket;
