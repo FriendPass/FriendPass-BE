88ceb292a089a06c58784965778ffb9b89f3cf70
baseballclub <abkimdoyo25@gmail.com>
2025-08-25 23:49:40 +0900

    .

--- src/main/java/com/likelion/friendpass/config/SecurityConfig.java
+++ src/main/java/com/likelion/friendpass/config/SecurityConfig.java
@@ -42,7 +42,8 @@ public class SecurityConfig {
                             "https://friendpass.site",
                             "https://www.friendpass.site",
                             "http://localhost:3000", // 개발용
-                            "http://localhost:3001" // 개발용
+                            "http://localhost:3001", // 개발용
+                            "https://friendpass-fe.netlify.app"
                     ));
                     cfg.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
                     cfg.setAllowedHeaders(List.of("Authorization","Content-Type","X-Requested-With"));
